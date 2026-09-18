e297b42cbadc2e946e6d4f30886f122a58bee41e
Denys <yakovden4k@gmail.com>
2026-09-18 19:31:48 +0300

    fddf

--- js/video-slides.js
+++ js/video-slides.js
@@ -1,27 +1,12 @@
 (function () {
   console.log('[VideoSlides] Script initialized for s6');
 
-  var VIDEO_MAP = {
-    '1.webp':  'https://itden-cdn.b-cdn.net/stretching/sdk23o.MP4',
-    '4.webp':  'https://itden-cdn.b-cdn.net/stretching/vd-2.mp4',
-    '8.webp':  'https://itden-cdn.b-cdn.net/stretching/vd-3.mp4',
-    '13.webp': 'https://itden-cdn.b-cdn.net/stretching/vd-4.mp4',
-    '18.webp': 'https://itden-cdn.b-cdn.net/stretching/video2.MP4',
-    '21.webp': 'https://itden-cdn.b-cdn.net/stretching/video4.MP4',
-    '24.webp': 'https://itden-cdn.b-cdn.net/stretching/Instagram_15.04.2026_MiddleSplit_Video.mp4',
-    '25.webp': 'https://itden-cdn.b-cdn.net/stretching/video5.MP4',
-    '27.webp': 'https://itden-cdn.b-cdn.net/stretching/Instagram_15.04.2026_FullSplit_Video.MP4',
-    '29.webp': 'https://itden-cdn.b-cdn.net/stretching/vd-10.mp4',
-    '31.webp': 'https://itden-cdn.b-cdn.net/stretching/feur289aew.mp4',
-    '32.webp': 'https://itden-cdn.b-cdn.net/stretching/vd-11.MP4',
-    '34.webp': 'https://itden-cdn.b-cdn.net/stretching/Splits%20Course%20from%20Zubalenok%20on%20IG.mp4'
-  };
 
   function initSwiper() {
     var gallerySwiperEl = document.querySelector('.s6-swiper');
     if (gallerySwiperEl) {
       var swiper = new Swiper(gallerySwiperEl, {
-        slidesPerView: 'auto',
+        slidesPerView: "auto",
         spaceBetween: 20,
         observer: true,          // Виправлення багу: змушує Swiper оновлюватись
         observeParents: true,    // при зміні розмірів картинок
